71b36814d4a61a3f66845a313d554c2a8bfcc166
Daniel Hateley <S4090151@student.rmit.edu.au>
2025-05-27 14:16:09 +1000

    File reading

--- roadregistry-assignment/src/main/java/rmit/assignment4/group127/App.java
+++ roadregistry-assignment/src/main/java/rmit/assignment4/group127/App.java
@@ -1,13 +1,17 @@
 package rmit.assignment4.group127;
 
+import java.io.FileNotFoundException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws FileNotFoundException {
+
         System.out.println( "Hello World!" );
+//        Person test = new Person();
+//        test.addPerson();
     }
 }
--- roadregistry-assignment/src/main/java/rmit/assignment4/group127/Person.java
+++ roadregistry-assignment/src/main/java/rmit/assignment4/group127/Person.java
@@ -1,6 +1,10 @@
 package rmit.assignment4.group127;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Date;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Person {
     private String personID;
@@ -11,7 +15,15 @@ public class Person {
     private HashMap<Date, Integer> demeritPoints;
     private boolean isSuspended;
 
-    public boolean addPerson() {
+    public boolean addPerson() throws FileNotFoundException {
+//        This code reads the data.txt tile - no searching
+//        String fileData;
+//        FileInputStream fileInput = null;
+//        Scanner fileScnr = null;
+//        fileInput = new FileInputStream("roadregistry-assignment/src/main/data.txt");
+//        fileScnr = new Scanner(fileInput);
+//        fileData = fileScnr.nextLine();
+//        System.out.println(fileData);
         return true;
     }
     public boolean updatePersonalDetails () {
