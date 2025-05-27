ce7c62e47c94037e7a825014dda7eb485dc03309
Zi-Fung <s4091279@student.rmit.edu.au>
2025-05-27 12:55:32 +1000

    Uploaded Maven Project
    Check if you have these files/folders
    - src (with main and test)
    - target (with test classes)
    - pom.xml file

---
+++ roadregistry-assignment/src/main/java/rmit/assignment4/group127/App.java
@@ -0,0 +1,13 @@
+package rmit.assignment4.group127;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ roadregistry-assignment/src/test/java/rmit/assignment4/group127/AppTest.java
@@ -0,0 +1,20 @@
+package rmit.assignment4.group127;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
